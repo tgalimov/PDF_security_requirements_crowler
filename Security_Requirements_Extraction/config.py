fb7dc088b4cf6dc038d5d2151a2c34b3162f8e93
tgalimov <35964502+tgalimov@users.noreply.github.com>
2021-06-15 14:36:28 +0300

    Update config.py

--- Security_Requirements_Extraction/config.py
+++ Security_Requirements_Extraction/config.py
@@ -1,4 +1,4 @@
-developerKey = "AIzaSyDDYOkM9cjUJcD_iXBPjmTQJERRD5hJ7H0" # use your own google api developer key https://console.cloud.google.com/apis/credentials
+developerKey = "developer_key" # use your own google api developer key https://console.cloud.google.com/apis/credentials
 search_text = "srs pdf"
 search_pages = 3
 input_source = "url_list"  # fill google or url_list value
